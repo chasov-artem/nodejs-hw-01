ed5dd82faaa6279da84b19226c7c8ae6c3c50d59
chasov-artem <chasov90@gmail.com>
2024-11-09 19:22:52 +0200

    fifth step

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    console.log('All contacts:', contacts);
+  } catch {
+    (error) => error.message;
+  }
+};
 
 console.log(await getAllContacts());
