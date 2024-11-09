a2162422e5e4ceef0c599f0d2d0a77fe55b6ab3b
chasov-artem <chasov90@gmail.com>
2024-11-09 19:42:27 +0200

    last step

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (!contacts.length) {
+      return console.log('No contacts to remove.');
+    } else {
+      contacts.pop();
+      await writeContacts(contacts);
+    }
+  } catch {
+    (error) => error.message;
+  }
+};
 
 removeLastContact();
